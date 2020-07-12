e7521898a92260814d1ec64f65dec6eabac2cc8a
Pavel Vorobyov <p.vorobyov@corp.mail.ru>
2020-07-12 11:13:52 +0300

    user subscription

--- ask/models/__init__.py
+++ ask/models/__init__.py
@@ -4,3 +4,4 @@ from .tag import Tag
 from .vote import Vote
 from .post import Question, Answer, Comment
 from .tag_subscription import TagSubscription
+from .user_subscription import UserSubscription
