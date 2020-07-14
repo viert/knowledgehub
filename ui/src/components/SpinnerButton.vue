cc23ca51aee4ac0f5959da099ea3cba5c0d3c9ec
Pavel Vorobyov <p.vorobyov@corp.mail.ru>
2020-07-14 11:46:04 +0300

    tag subscription

---
+++ ui/src/components/SpinnerButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button v-on="$listeners" :disabled="loading">
+    <i v-if="loading" class="fas fa-spinner fa-spin"></i>
+    <slot v-else></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
