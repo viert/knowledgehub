65520debf306f30cf2a76a5bd125586af3556420
Pavel Vorobyov <p.vorobyov@corp.mail.ru>
2020-07-18 20:36:53 +0300

    more components

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -4,10 +4,14 @@ import router from './router'
 import store from './store'
 import Fragment from 'vue-fragment'
 import Tag from '@/components/Tag.vue'
+import Progress from '@/components/Progress.vue'
+import Voter from '@/components/Voter.vue'
 
 Vue.use(Fragment.Plugin)
 Vue.config.productionTip = false
 Vue.component('Tag', Tag)
+Vue.component('Progress', Progress)
+Vue.component('Voter', Voter)
 
 new Vue({
   router,
