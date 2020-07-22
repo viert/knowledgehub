247e547e333671d22f194fcbbee03e0b1334d444
Maria <m.vorobyeva@corp.mail.ru>
2020-07-22 22:16:42 +0300

    add post actions

---
+++ ui/src/components/PostActions.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="post-"
+</template>
+
+
+
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
+@Component
+export default class PostActions extends Vue {
+  @Prop({ type: String, required: true }) readonly body!: string
+  @Prop({ type: Boolean, default: false }) readonly strict!: boolean
+  @Prop({ type: Boolean, default: false }) readonly inline!: boolean
+
+
+}
+</script>
