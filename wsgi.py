3e70ac9090e2935604eed01c2826dfc5d5742bee
Воробьев Павел <p.vorobyov@corp.mail.ru>
2020-08-04 20:00:55 +0300

    proper libraries in wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,12 @@
 import os
 from cachelib import SimpleCache
-from uengine.context import ctx
+from glasskit import ctx
 from ask import app, force_init_app
 
 force_init_app()
 
 if isinstance(ctx.cache, SimpleCache):
-    if not os.getenv("UENGINE_USE_SIMPLE_CACHE"):
+    if not os.getenv("GLASSKIT_USE_SIMPLE_CACHE"):
         raise RuntimeError("""Running your application under uwsgi control without centralized cache
 is dangerous. If you rely on cache invalidation your app instances will be
 able to invalidate their own in-memory cache while others will consider the
@@ -14,7 +14,7 @@ same cache keys still valid.
 
 If you're sure it's OK for your project please set:
  
-UENGINE_USE_SIMPLE_CACHE=1
+GLASSKIT_USE_SIMPLE_CACHE=1
 
 """)
 
